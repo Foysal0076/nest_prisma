7d10c3bd3b746f0a98b7e232cad10561fe2b0521
Foysal Ahmed <foysalxahmed@gmail.com>
2023-01-20 07:17:08 +0600

    Custom decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, Req } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common'
 import { Request } from 'express'
 import { AuthService } from 'src/auth/auth.service'
 import { SignupDto } from 'src/auth/dto'
@@ -12,6 +19,7 @@ export class AuthController {
   //Instead of doing above we can do the followings
   constructor(private authService: AuthService) {}
 
+  //This will automatically send a 201 response
   @Post('signup')
   signUp(@Body() signUpDto: SignupDto) {
     //req is the Request object of express
@@ -19,6 +27,7 @@ export class AuthController {
     return this.authService.signUp(signUpDto)
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() loginDto: SignupDto) {
     return this.authService.login(loginDto)
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,14 @@
-import {  Controller, Get, Req, UseGuards } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Req,
+  UseGuards,
+} from '@nestjs/common'
+import { User } from '@prisma/client'
 import { Request } from 'express'
+import { GetUser } from 'src/auth/decorator/get-user.decorator'
 import { JwtAuthGuard } from 'src/auth/guard'
 import { UserService } from 'src/user/user.service'
 
@@ -7,9 +16,14 @@ import { UserService } from 'src/user/user.service'
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @HttpCode(HttpStatus.OK)
   @UseGuards(JwtAuthGuard)
   @Get('details')
-  getUserDetails(@Req() req: Request) {
-    return this.userService.getUserDetails(req.user)
+  // getUserDetails(@Req() req: Request) {
+  //   return this.userService.getUserDetails(req.user)
+  // }
+  getUserDetails(@GetUser() user: User, @GetUser('email') email: string) {
+    console.log(email)
+    return this.userService.getUserDetails(user)
   }
 }
