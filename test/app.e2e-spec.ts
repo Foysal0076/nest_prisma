bbf0074e74d44df2fb1ba344f7d00eca0d0296bb
Foysal Ahmed <foysalxahmed@gmail.com>
2023-01-21 16:25:15 +0600

    Setup environments for test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,24 @@
+import { INestApplication } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import { AppModule } from '../src/app.module'
 
 describe('App e2e', () => {
+  let app: INestApplication
   beforeAll(async () => {
+    //Simulate the server
     const module = await Test.createTestingModule({
       imports: [AppModule],
     }).compile()
+    app = module.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+    await app.init()
   })
+
+  //teardown logic
+  afterAll(() => {
+    app.close()
+  })
+
   it.todo('pass')
 })
